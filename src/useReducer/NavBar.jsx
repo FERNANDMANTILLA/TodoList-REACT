a7b1b5076088e3b1dd60fca9f0d9ac1b0218bec8
PaoloPG2705 <paolomta2705@gmail.com>
2022-06-29 19:08:27 +0200

    last

--- src/useReducer/NavBar.jsx
+++ src/useReducer/NavBar.jsx
@@ -5,9 +5,9 @@ export const NavBar = () => {
   return (
     <nav className="nav nav-tabs gap-5 justify-content-lg-start justify-content-center text-left bg-gray-dark p-1 shadow-box-2 mb-3 ">
       
-      <Link className='nav-link' to='/'><p className='m-0 text-white '>All</p></Link>
-      <Link className='nav-link' to='completed'><p className='m-0 text-white '>Completed</p></Link>
-      <Link className='nav-link' to='pendings'><p className='m-0 text-white '>Pendings</p></Link>
+      <Link className='nav-link' to='TodoList-REACT/'><p className='m-0 text-white '>All</p></Link>
+      <Link className='nav-link' to='TodoList-REACT/completed'><p className='m-0 text-white '>Completed</p></Link>
+      <Link className='nav-link' to='TodoList-REACT/pendings'><p className='m-0 text-white '>Pendings</p></Link>
      
     </nav>
   )
--- src/useReducer/Routes/Router.jsx
+++ src/useReducer/Routes/Router.jsx
@@ -10,8 +10,8 @@ export const Router = () => {
     <>
             <Routes>
              <Route path="TodoList-REACT/" element={<Page type='TODO LIST' todos={todos} handleDeleteTodo={handleDeleteTodo} handleToggleToDo={handleToggleToDo} handleNewTodo={handleNewTodo}  />} />
-             <Route path="TodoList-REACT/pendings" element={<Page type='PENDING TODO LIST' todos={allFalse} handleDeleteTodo={handleDeleteTodo} handleToggleToDo={handleToggleToDo} handleNewTodo={handleNewTodo}  />} />
-             <Route path="TodoList-REACT/completed" element={<Page type='COMPLETED TODO LIST' todos={allTrue} handleDeleteTodo={handleDeleteTodo} handleToggleToDo={handleToggleToDo} handleNewTodo={handleNewTodo}  />} />
+             <Route path="/TodoList-REACT/pendings" element={<Page type='PENDING TODO LIST' todos={allFalse} handleDeleteTodo={handleDeleteTodo} handleToggleToDo={handleToggleToDo} handleNewTodo={handleNewTodo}  />} />
+             <Route path="/TodoList-REACT/completed" element={<Page type='COMPLETED TODO LIST' todos={allTrue} handleDeleteTodo={handleDeleteTodo} handleToggleToDo={handleToggleToDo} handleNewTodo={handleNewTodo}  />} />
              
             </Routes>
     </>
